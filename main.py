25fd9579642b9ba595bef18eda92997c6dc6a462
Andrei Karl Nagba <finitydrei@gmail.com>
2024-08-22 11:47:39 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# FNumber = input("First Number:")
+# SNumber = input ("Second Number:")
+#
+# if FNumber > SNumber:
+#     print(f"{FNumber} is greater than {SNumber}")
+# elif  FNumber< SNumber:
+#     print(f"{FNumber} is less than {SNumber} ")
+# elif FNumber == SNumber:
+#     print(f"{FNumber} is equals to {SNumber}")
+
+
+CM1 = input("Enter classmate name 1:")
+CM2 = input ("Enter classmate name 2:")
+CM3 = input ("Enter classmate name 3:")
+
+print(f"Your classmates are: {CM1} ,{CM2} x, & {CM3} ")
